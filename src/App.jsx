f3d76a6da7ad24530049857e7bc724bd04d65a62
Wegenke <matthew.wegenke@gmail.com>
2025-12-17 15:55:57 -0600

    removed unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import NavBar from './Components/displayComponents/NavBar'
 import RouterController from './Components/navigation/RouterController'
 import SiteMap from './Components/displayComponents/SiteMap'
-import { useEffect } from 'react'
 import AppProvider from './Components/contextComponents/AppContext';
-
 
 const  App = () => {
   return (
